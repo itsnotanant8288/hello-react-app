05530509b0993fcb48ed2bdc7a5551568548ab96
Anant Singh <anantsingh8288@gmail.com>
2023-09-15 11:22:01 +0530

    basic request of axios post in signup working

--- src/modules/SignUp.js
+++ src/modules/SignUp.js
@@ -1,70 +1,108 @@
-import { ErrorMessage, Field, Form, Formik } from "formik";
 import React, { useState } from "react";
-import * as Yup from "yup";
 import axios from "axios";
 
 const SignUp = () => {
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        password: '',
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    phone_number: "",
+    age: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post("http://127.0.0.1:3031/api/users", {
+        user_name: formData.name,
+        email: formData.email,
+        password: formData.password,
+        phone_number: formData.phone_number,
+        age: formData.age,
       });
-    
-      const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({
-          ...formData,
-          [name]: value,
-        });
-      };
-    
-      const handleSubmit = (e) => {
-        e.preventDefault();
-        // You can add your signup logic here, like sending data to a server
-        console.log('Form data submitted:', formData);
-      };
-    
-      return (
+
+      if (response.status === 200) {
+        console.log("User created successfully:", response.data);
+        // You can handle success here, e.g., redirect to a different page
+      }
+    } catch (error) {
+      console.error("Error creating user:", error);
+      // Handle error, e.g., display an error message to the user
+    }
+  };
+
+  return (
+    <div>
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSubmit}>
         <div>
-          <h2>Sign Up</h2>
-          <form onSubmit={handleSubmit}>
-            <div>
-              <label htmlFor="name">Name:</label>
-              <input
-                type="text"
-                id="name"
-                name="name"
-                value={formData.name}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div>
-              <label htmlFor="email">Email:</label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div>
-              <label htmlFor="password">Password:</label>
-              <input
-                type="password"
-                id="password"
-                name="password"
-                value={formData.password}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <button type="submit">Sign Up</button>
-          </form>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
         </div>
-      );
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="phone_number">Phone Number:</label>
+          <input
+            type="text"
+            id="phone_number"
+            name="phone_number"
+            value={formData.phone_number}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="age">Age:</label>
+          <input
+            type="number"
+            id="age"
+            name="age"
+            value={formData.age}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
 };
 
 export default SignUp;
